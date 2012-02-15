7f8d04ebf10d9871a84f2b0fc79c44c457db4cba
Laurent <lcontzen@gmail.com>
2012-02-15 01:48:09 +0100

    New view page/bals

---
+++ application/views/page/bals.php
@@ -0,0 +1,53 @@
+<div class="page-header">
+  <h1>Agenda des Bals</h1>
+</div>
+<h3>Février</h3>
+<div class="row">
+  <div class="span4" align="right">
+	<p>
+	  <b>Vendredi 10 Février :</b> <br />
+	  <br />
+	  <b>Vendredi 17 Février :</b> <br />
+	  <b>Vendredi 24 Février :</b>
+	</p>
+  </div>
+  <div class="span5">
+	<p>
+	  Agro <br />
+	  CELB <br />
+	  Philo  <br />
+	  CJC <br />
+	  CP
+	</p>
+  </div>
+</div>
+
+<h3>Mars</h3>
+<div class="row">
+  <div class="span4" align="right">
+	<p>
+	  <b>Vendredi 02 Mars :</b> <br />
+	  <br />
+	  <b>Samedi 03 Mars : </b> <br />
+	  <b>Vendredi 09 Mars :</b> <br />
+	  <br />
+	  <b>Samedi 10 Mars : </b> <br />
+	  <b>Vendredi 23 Mars :</b> <br />
+	  <br />
+	  <b>Vendredi 30 Mars :</b>
+	</p>
+  </div>
+  <div class="span5">
+	<p>
+	  CdS <br />
+	  Droit <br />
+	  Pharma <br />
+	  CPS  <br />
+	  CIGA <br />
+	  CM <br />
+	  CHAA/CdH/CROM <br />
+	  ISEP/KINE <br />
+	  CARé
+	</p>
+  </div>
+</div>
