9c71f0cb7d17c62f30445ee8fd786642d4c11bf6
Laurent <lcontzen@gmail.com>
2012-02-15 11:33:38 +0100

    Little changes in page/comite view

--- application/views/page/comite.php
+++ application/views/page/comite.php
@@ -8,19 +8,19 @@
 	  <h3>Président</h3>
 	  <p><?php echo HTML::image('public/pics/comite1112/adrien.jpg', array('alt' => "Président", 'width' => '120', 'height' => '170')); ?></p>
 	  <p><b>Adrien Leleu</b> <br />
-		<?php echo HTML::mailto("president@ace-ulb.org");	?> </p>
+		<?php echo HTML::mailto("president@ace-ulb.be");	?> </p>
 	</div>
 	<div class="span5">
 	  <h3>Vice-Président Interne</h3>
 	  <p><?php echo HTML::image('public/pics/comite1112/arthur.jpg', array('alt' => "Vice-Président Interne", 'width' => '120', 'height' => '170')); ?></p>
 	  <p><b>Arthur Ooghe</b><br />
-		<?php echo HTML::mailto("vpi@ace-ulb.org");	?> </p>
+		<?php echo HTML::mailto("vpi@ace-ulb.be");	?> </p>
 	</div>
 	<div class="span5">
 	  <h3>Vice-Présidente Externe</h3>
 	  <p><?php echo HTML::image('public/pics/comite1112/marine.jpg', array('alt' => "Vice-Présidente Externe", 'width' => '120', 'height' => '170')); ?></p>
 	  <p><b>Marine Anciaux</b><br />
-		<?php echo HTML::mailto("vpe@ace-ulb.org");	?> </p>
+		<?php echo HTML::mailto("vpe@ace-ulb.be");	?> </p>
 	</div>	
   </div>
   
@@ -29,19 +29,19 @@
 	  <h3>Trésorier</h3>
 	  <p><?php echo HTML::image('public/pics/comite1112/jerome.jpg', array('alt' => "Trésorier", 'width' => '120', 'height' => '170')); ?></p>
 	  <p><b>Jérôme Malengret </b><br />
-		<?php echo HTML::mailto("tresorier@ace-ulb.org");	?> </p>
+		<?php echo HTML::mailto("tresorier@ace-ulb.be");	?> </p>
 	</div>
 	<div class="span5">
 	  <h3>Secrétaire</h3>
 	  <p><?php echo HTML::image('public/pics/comite1112/louis.jpg', array('alt' => "Secrétaire", 'width' => '120', 'height' => '170')); ?></p>
 	  <p><b>Louis Bersini</b><br />
-		<?php echo HTML::mailto("secretaire@ace-ulb.org");	?> </p>
+		<?php echo HTML::mailto("secretaire@ace-ulb.be");	?> </p>
 	</div>
 	<div class="span5">
 	  <h3>Trésorière Folklore</h3>
 	  <p><?php echo HTML::image('public/pics/comite1112/julie.jpg', array('alt' => "Trésorière Folklore", 'width' => '120', 'height' => '170')); ?></p>
 	  <p><b>Julie Berthelier</b><br />
-		<?php echo HTML::mailto("tresorierfolklore@ace-ulb.org");	?> </p>
+		<?php echo HTML::mailto("tresorier.folklore@ace-ulb.be");	?> </p>
 	</div>
   </div>
   
@@ -50,19 +50,20 @@
 	  <h3>Folklore 1</h3>
 	  <p><?php echo HTML::image('public/pics/comite1112/will.jpg', array('alt' => "Folklore 1", 'width' => '120', 'height' => '170')); ?></p>
 	  <p><b>William Wright Blanes </b><br />
-		<?php echo HTML::mailto("folklore1@ace-ulb.org");	?> </p>
+		<?php echo HTML::mailto("folklore@ace-ulb.be");	?> </p>
 	</div>
 	<div class="span5">
 	  <h3>Folklore 2</h3>
 	  <p><?php echo HTML::image('public/pics/comite1112/quentin.jpg', array('alt' => "Folklore 2", 'width' => '120', 'height' => '170')); ?></p>
 	  <p><b>Quentin Caputi</b><br />
-		<?php echo HTML::mailto("folklore2@ace-ulb.org");	?> </p>
+		<?php echo HTML::mailto("folklore2@ace-ulb.be");	?> </p>
 	</div>
 	<div class="span5">
 	  <h3>Culture/Hérésie</h3>
-	  <p><?php echo HTML::image('public/pics/comite1112/nicolas.jpg', array('alt' => "Culture/Hérésie", 'width' => '120', 'height' => '170')); ?></p>
-	  <p><b>Nicolas Finet</b><br />
-		<?php echo HTML::mailto("culture@ace-ulb.org");	?> </p>
+	  <p><?php echo HTML::image('public/pics/comite1112/olivier.jpg', array('alt' => "Culture/Hérésie", 'width' => '120', 'height' => '170')); ?></p>
+	  <p><b>Olivier Moisse</b><br />
+		<?php echo HTML::mailto("culture@ace-ulb.be");	?> <br />
+		<?php echo HTML::mailto("heresie@ace-ulb.be");	?> </p>
 	</div>
   </div>
   
@@ -71,19 +72,20 @@
 	  <h3>Evénements 1</h3>
 	  <p><?php echo HTML::image('public/pics/comite1112/gaelle.jpg', array('alt' => "Evenements 1", 'width' => '120', 'height' => '170')); ?></p>
 	  <p><b>Gaëlle Moury </b><br />
-		<?php echo HTML::mailto("evenements1@ace-ulb.org");	?> </p>
+		<?php echo HTML::mailto("evenements1@ace-ulb.be");	?> </p>
 	</div>
 	<div class="span5">
 	  <h3>Evénements 2</h3>
 	  <p><?php echo HTML::image('public/pics/comite1112/lyn.jpg', array('alt' => "Evénements 2", 'width' => '120', 'height' => '170')); ?></p>
 	  <p><b>Lyn Schmitz</b><br />
-		<?php echo HTML::mailto("evenements2@ace-ulb.org");	?> </p>
+		<?php echo HTML::mailto("evenements2@ace-ulb.be");	?> </p>
 	</div>
 	<div class="span5">
 	  <h3>Social/Librex</h3>
 	  <p><?php echo HTML::image('public/pics/comite1112/zoe.jpg', array('alt' => "Social/Librex", 'width' => '120', 'height' => '170')); ?></p>
 	  <p><b>Zoé Gennaux</b><br />
-		<?php echo HTML::mailto("social@ace-ulb.org");	?> </p>
+		<?php echo HTML::mailto("social@ace-ulb.be");	?> <br />
+		<?php echo HTML::mailto("librex@ace-ulb.be");	?> </p>
 	</div>
   </div>
   
@@ -92,19 +94,20 @@
 	  <h3>Archives/Photos</h3>
 	  <p><?php echo HTML::image('public/pics/comite1112/florence.jpg', array('alt' => "Archives/Photos", 'width' => '120', 'height' => '170')); ?></p>
 	  <p><b>Florence Lestienne </b><br />
-		<?php echo HTML::mailto("archives@ace-ulb.org");	?> </p>
+		<?php echo HTML::mailto("archives@ace-ulb.be");	?> <br />
+		<?php echo HTML::mailto("photos@ace-ulb.be");	?> </p>
 	</div>
 	<div class="span5">
 	  <h3>Environnement</h3>
 	  <p><?php echo HTML::image('public/pics/comite1112/emilie.jpg', array('alt' => "Environnement", 'width' => '120', 'height' => '170')); ?></p>
 	  <p><b>Emilie Redant</b><br />
-		<?php echo HTML::mailto("environnement@ace-ulb.org");	?> </p>
+		<?php echo HTML::mailto("environnement@ace-ulb.be");	?> </p>
 	</div>
 	<div class="span5">
 	  <h3>Web</h3>
 	  <p><?php echo HTML::image('public/pics/comite1112/laurent.jpg', array('alt' => "Web", 'width' => '120', 'height' => '170')); ?></p>
 	  <p><b>Laurent Contzen</b><br />
-		<?php echo HTML::mailto("web@ace-ulb.org");	?> </p>
+		<?php echo HTML::mailto("webmaster@ace-ulb.be");	?> </p>
 	</div>
   </div>
 </div>
