b878530238bbda788f5b9b93834111c85fba850d
Laurent <lcontzen@gmail.com>
2012-02-15 08:17:57 +0100

    New view page/ntv

---
+++ application/views/page/ntv.php
@@ -0,0 +1,24 @@
+<div class="page-header">
+  <h1>Nuit Théodore Verhaegen</h1>
+</div>
+<div class="row">
+  <div class="span11">
+	<p>L'Association des Cercles Etudiants, le Cercle du Libre-Examen l'Union des Anciens Etudiants et l'Ordre Théodore Verhaegen ont le plaisir de vous convier à sa NUIT THEODORE VERHAEGEN dans le cadre superbement restauré de l'ARSENAL d'Etterbeek ! </p>
+	<p>Précédée du banquet (complet) à 19h, la soirée débutera à 22h <br />
+	  Préventes :
+	  <ul>
+		<li>Etudiant : 9 € (+ 2€ sur place)</li>
+		<li>Bourgeois : 11 € (+ 2€ sur place)</li>
+	  </ul>
+	  Préventes disponibles : <br />
+	  <ul>
+		<li>De 12h à 16h, au bâtiment F1</li>	
+		<li>De 18h à 19h, au local de l'ACE</li>	
+	  </ul>
+	  Les non-étudiants peuvent également réserver leur prévente par mail (nuit.theodore.verhaegen@gmail.com) jusqu'au vendredi 2/12, 18h. Elles peuvent ensuite être payées sur le compte BE61 001-0810667-17.
+	</p>
+  </div>
+  <div class="span5">
+	<?php echo HTML::image('public/images/ntv11big.jpg', array('alt' => 'Affiche NTV 2011', 'width' => '375')); ?>
+  </div>  
+</div>
