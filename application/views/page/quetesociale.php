7ffb3524843719e9b185ec011e4c6cd7b11e8633
Laurent <lcontzen@gmail.com>
2012-02-15 08:14:52 +0100

    New view page/quetesociale

---
+++ application/views/page/quetesociale.php
@@ -0,0 +1,32 @@
+<div class="page-header">
+  <h1>Quête Sociale</h1>
+</div>
+<div class="row">
+  <div class="span11">
+	<p>Depuis de nombreuses années, à l'approche de la Saint-V, les étudiants fraîchement baptisés partent à l'assaut des passants et automobilistes pour solliciter quelques oboles. L'origine de cette tradition est très lointaine et incertaine. Mais, lorsque nous retraçons l'histoire de la Saint-V, nous découvrons qu'en 1939, l'Association Générale des Etudiants (ancêtre de l'actuelle Association des Cercles Etudiants) décide de ne pas organiser le traditionnel cortège et de le remplacer par une collecte de fonds au profit des familles de soldats mobilisés au front ; elle en fait de même en 1940 et 1941, juste avant la fermeture de l'Université Libre de Bruxelles... </p>
+	
+	<p>C'est de cet épisode qu'est née, voici quelques années, l'idée de consacrer une journée en faveur d'une oeuvre sociale et, depuis 2009, c'est l'opération CAP 48 qui bénéfice du soutien de cette "Quête sociale" organisée par l'Association des Cercles Etudiants. Grâce à la forte mobilisation des étudiants et à la générosité des passants et automobilistes, pas moins de 3.800 € en 2009 et 4.200 € en 2010 ont été récoltés et versés à CAP 48.</p>
+	
+	<p>En pratique, le <b>lundi 14 novembre</b> prochain, des étudiants baptisés ou non, anciens ou nouveaux, seront postés à différents carrefours de la capitale :
+	  <ul>
+		<li>Gare d'Etterbeek, </li>
+		<li>Avenue Franklin Roosevelt, </li>
+		<li>Boulevard Général Jacques, </li>
+		<li>Avenue Louise, </li>
+		<li>Porte de Namur, </li>
+		<li>Delta, </li>
+		<li>Campus d'Erasme, </li>
+		<li>Cimetière d'Ixelles, </li>
+		<li>... </li>
+	  </ul>
+	  Dès 8h et jusqu'à 20 heures, ils y seront visibles grâce à des banderoles CAP 48 et des badges d'accréditation. Cette année encore, réservez-leur un bon accueil et faites en don en faveur de CAP 48 ! Merci ! </p>
+	
+	<p>Pour tout autre renseignement n’hésitez pas à nous contacter. </p> 
+	
+	<p>Zoé GENNAUX, Déléguée sociale de l’Association des Cercles Etudiants <br />
+	  Adrien LELEU, Président de l'Association des Cercles Etudiants</p>
+  </div>
+  <div class="span5">
+	<?php echo HTML::image('public/images/cap48.jpg', array('alt' => 'Logo Cap48')); ?>
+  </div>
+</div>
