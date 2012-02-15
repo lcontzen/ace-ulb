77557f67e2489ede4709e9d308ae546a9ed0091d
Laurent <lcontzen@gmail.com>
2012-02-15 09:31:52 +0100

    New view page/links

---
+++ application/views/page/links.php
@@ -0,0 +1,201 @@
+<div class="page-header">
+  <h1>Liens</h1>
+</div>
+<div class="row" align="center">
+  <div class="span4">
+   	<p><?php $cercle = "agro";
+	  echo HTML::anchor($cercles[$cercle]["web"],
+						HTML::image($cercles[$cercle]["logo"], array('alt' => 'Logo ' . $cercles[$cercle]["name"], 'height' => '125'))
+						. "<br /> " . $cercles[$cercle]["name"]); ?></p>
+  </div>
+  <div class="span4">
+   	<p><?php $cercle = "care";
+	  echo HTML::anchor($cercles[$cercle]["web"],
+						HTML::image($cercles[$cercle]["logo"], array('alt' => 'Logo ' . $cercles[$cercle]["name"], 'height' => '125'))
+						. "<br /> " . $cercles[$cercle]["name"]); ?></p>
+  </div>
+  <div class="span4">
+   	<p><?php $cercle = "cd";
+	  echo HTML::anchor($cercles[$cercle]["web"],
+						HTML::image($cercles[$cercle]["logo"], array('alt' => 'Logo ' . $cercles[$cercle]["name"], 'height' => '125'))
+						. "<br /> " . $cercles[$cercle]["name"]); ?></p>
+  </div>
+  <div class="span4">
+   	<p><?php $cercle = "cdh";
+	  echo HTML::anchor($cercles[$cercle]["web"],
+						HTML::image($cercles[$cercle]["logo"], array('alt' => 'Logo ' . $cercles[$cercle]["name"], 'height' => '125'))
+						. "<br /> " . $cercles[$cercle]["name"]); ?></p>
+  </div>
+</div>
+
+<div class="row" align="center">
+  <div class="span4">
+   	<p><?php $cercle = "cds";
+	  echo HTML::anchor($cercles[$cercle]["web"],
+						HTML::image($cercles[$cercle]["logo"], array('alt' => 'Logo ' . $cercles[$cercle]["name"], 'height' => '125'))
+						. "<br /> " . $cercles[$cercle]["name"]); ?></p>
+  </div>
+  <div class="span4">
+   	<p><?php $cercle = "cebulb";
+	  echo HTML::anchor($cercles[$cercle]["web"],
+						HTML::image($cercles[$cercle]["logo"], array('alt' => 'Logo ' . $cercles[$cercle]["name"], 'height' => '125'))
+						. "<br /> " . $cercles[$cercle]["name"]); ?></p>
+  </div>
+  <div class="span4">
+   	<p><?php $cercle = "cecs";
+	  echo HTML::anchor($cercles[$cercle]["web"],
+						HTML::image($cercles[$cercle]["logo"], array('alt' => 'Logo ' . $cercles[$cercle]["name"], 'height' => '125'))
+						. "<br /> " . $cercles[$cercle]["name"]); ?></p>
+  </div>
+  <div class="span4">
+   	<p><?php $cercle = "celb";
+	  echo HTML::anchor($cercles[$cercle]["web"],
+						HTML::image($cercles[$cercle]["logo"], array('alt' => 'Logo ' . $cercles[$cercle]["name"], 'height' => '125'))
+						. "<br /> " . $cercles[$cercle]["name"]); ?></p>
+  </div>
+</div>
+
+<div class="row" align="center">
+  <div class="span4">
+   	<p><?php $cercle = "cepha";
+	  echo HTML::anchor($cercles[$cercle]["web"],
+						HTML::image($cercles[$cercle]["logo"], array('alt' => 'Logo ' . $cercles[$cercle]["name"], 'height' => '125'))
+						. "<br /> " . $cercles[$cercle]["name"]); ?></p>
+  </div>
+  <div class="span4">
+   	<p><?php $cercle = "cgeo";
+	  echo HTML::anchor($cercles[$cercle]["web"],
+						HTML::image($cercles[$cercle]["logo"], array('alt' => 'Logo ' . $cercles[$cercle]["name"], 'height' => '125'))
+						. "<br /> " . $cercles[$cercle]["name"]); ?></p>
+  </div>
+  <div class="span4">
+   	<p><?php $cercle = "chaa";
+	  echo HTML::anchor($cercles[$cercle]["web"],
+						HTML::image($cercles[$cercle]["logo"], array('alt' => 'Logo ' . $cercles[$cercle]["name"], 'height' => '125'))
+						. "<br /> " . $cercles[$cercle]["name"]); ?></p>
+  </div>
+  <div class="span4">
+   	<p><?php $cercle = "chimacienne";
+	  echo HTML::anchor($cercles[$cercle]["web"],
+						HTML::image($cercles[$cercle]["logo"], array('alt' => 'Logo ' . $cercles[$cercle]["name"], 'height' => '125'))
+						. "<br /> " . $cercles[$cercle]["name"]); ?></p>
+  </div>
+</div>
+
+<div class="row" align="center">
+  <div class="span4">
+   	<p><?php $cercle = "ci";
+	  echo HTML::anchor($cercles[$cercle]["web"],
+						HTML::image($cercles[$cercle]["logo"], array('alt' => 'Logo ' . $cercles[$cercle]["name"], 'height' => '125'))
+						. "<br /> " . $cercles[$cercle]["name"]); ?></p>
+  </div>
+  <div class="span4">
+   	<p><?php $cercle = "ciga";
+	  echo HTML::anchor($cercles[$cercle]["web"],
+						HTML::image($cercles[$cercle]["logo"], array('alt' => 'Logo ' . $cercles[$cercle]["name"], 'height' => '125'))
+						. "<br /> " . $cercles[$cercle]["name"]); ?></p>
+  </div>
+  <div class="span4">
+   	<p><?php $cercle = "cjc";
+	  echo HTML::anchor($cercles[$cercle]["web"],
+						HTML::image($cercles[$cercle]["logo"], array('alt' => 'Logo ' . $cercles[$cercle]["name"], 'height' => '125'))
+						. "<br /> " . $cercles[$cercle]["name"]); ?></p>
+  </div>
+  <div class="span4">
+   	<p><?php $cercle = "ck";
+	  echo HTML::anchor($cercles[$cercle]["web"],
+						HTML::image($cercles[$cercle]["logo"], array('alt' => 'Logo ' . $cercles[$cercle]["name"], 'height' => '125'))
+						. "<br /> " . $cercles[$cercle]["name"]); ?></p>
+  </div>
+</div>
+
+<div class="row" align="center">
+  <div class="span4">
+   	<p><?php $cercle = "cm";
+	  echo HTML::anchor($cercles[$cercle]["web"],
+						HTML::image($cercles[$cercle]["logo"], array('alt' => 'Logo ' . $cercles[$cercle]["name"], 'height' => '125'))
+						. "<br /> " . $cercles[$cercle]["name"]); ?></p>
+  </div>
+  <div class="span4">
+   	<p><?php $cercle = "cp";
+	  echo HTML::anchor($cercles[$cercle]["web"],
+						HTML::image($cercles[$cercle]["logo"], array('alt' => 'Logo ' . $cercles[$cercle]["name"], 'height' => '125'))
+						. "<br /> " . $cercles[$cercle]["name"]); ?></p>
+  </div>
+  <div class="span4">
+   	<p><?php $cercle = "cpl";
+	  echo HTML::anchor($cercles[$cercle]["web"],
+						HTML::image($cercles[$cercle]["logo"], array('alt' => 'Logo ' . $cercles[$cercle]["name"], 'height' => '125'))
+						. "<br /> " . $cercles[$cercle]["name"]); ?></p>
+  </div>
+  <div class="span4">
+   	<p><?php $cercle = "cps";
+	  echo HTML::anchor($cercles[$cercle]["web"],
+						HTML::image($cercles[$cercle]["logo"], array('alt' => 'Logo ' . $cercles[$cercle]["name"], 'height' => '125'))
+						. "<br /> " . $cercles[$cercle]["name"]); ?></p>
+  </div>
+</div>
+
+<div class="row" align="center">
+  <div class="span4">
+   	<p><?php $cercle = "cpsy";
+	  echo HTML::anchor($cercles[$cercle]["web"],
+						HTML::image($cercles[$cercle]["logo"], array('alt' => 'Logo ' . $cercles[$cercle]["name"], 'height' => '125'))
+						. "<br /> " . $cercles[$cercle]["name"]); ?></p>
+  </div>
+  <div class="span4">
+   	<p><?php $cercle = "crom";
+	  echo HTML::anchor($cercles[$cercle]["web"],
+						HTML::image($cercles[$cercle]["logo"], array('alt' => 'Logo ' . $cercles[$cercle]["name"], 'height' => '125'))
+						. "<br /> " . $cercles[$cercle]["name"]); ?></p>
+  </div>
+  <div class="span4">
+   	<p><?php $cercle = "cs";
+	  echo HTML::anchor($cercles[$cercle]["web"],
+						HTML::image($cercles[$cercle]["logo"], array('alt' => 'Logo ' . $cercles[$cercle]["name"], 'height' => '125'))
+						. "<br /> " . $cercles[$cercle]["name"]); ?></p>
+  </div>
+  <div class="span4">
+   	<p><?php $cercle = "fronta";
+	  echo HTML::anchor($cercles[$cercle]["web"],
+						HTML::image($cercles[$cercle]["logo"], array('alt' => 'Logo ' . $cercles[$cercle]["name"], 'height' => '125'))
+						. "<br /> " . $cercles[$cercle]["name"]); ?></p>
+  </div>
+</div>
+
+<div class="row" align="center">
+  <div class="span4">
+   	<p><?php $cercle = "isep";
+	  echo HTML::anchor($cercles[$cercle]["web"],
+						HTML::image($cercles[$cercle]["logo"], array('alt' => 'Logo ' . $cercles[$cercle]["name"], 'height' => '125'))
+						. "<br /> " . $cercles[$cercle]["name"]); ?></p>
+  </div>
+  <div class="span4">
+   	<p><?php $cercle = "lux";
+	  echo HTML::anchor($cercles[$cercle]["web"],
+						HTML::image($cercles[$cercle]["logo"], array('alt' => 'Logo ' . $cercles[$cercle]["name"], 'height' => '125'))
+						. "<br /> " . $cercles[$cercle]["name"]); ?></p>
+  </div>
+  <div class="span4">
+   	<p><?php $cercle = "librex";
+	  echo HTML::anchor($cercles[$cercle]["web"],
+						HTML::image($cercles[$cercle]["logo"], array('alt' => 'Logo ' . $cercles[$cercle]["name"], 'height' => '125'))
+						. "<br /> " . $cercles[$cercle]["name"]); ?></p>
+  </div>
+  <div class="span4">
+   	<p><?php $cercle = "nam";
+	  echo HTML::anchor($cercles[$cercle]["web"],
+						HTML::image($cercles[$cercle]["logo"], array('alt' => 'Logo ' . $cercles[$cercle]["name"], 'height' => '125'))
+						. "<br /> " . $cercles[$cercle]["name"]); ?></p>
+  </div>
+</div>
+
+<div class="row" align="center">
+  <div class="span4">
+	<p><?php $cercle = "semeur";
+	  echo HTML::anchor($cercles[$cercle]["web"],
+						HTML::image($cercles[$cercle]["logo"], array('alt' => 'Logo ' . $cercles[$cercle]["name"], 'height' => '125'))
+						. "<br /> " . $cercles[$cercle]["name"]); ?></p>
+  </div>
+</div>
+
