2f86369993ba28d399d50abe05ed337299b5025d
Laurent <lcontzen@gmail.com>
2012-02-15 09:42:27 +0100

    New view page/heresie

---
+++ application/views/page/heresie.php
@@ -0,0 +1,69 @@
+<div class="page-header">
+  <h1>L'Hérésie <small>Le journal des cercles étudiants de l'ULB</small></h1>
+</div>
+
+<div class="row">
+  <div class="span11">
+	<h3>Hérésie n°2 <small>Février 2011</small></h3>
+	<h5>Sommaire :</h5>
+	<ul>
+	  <li> Edito du Président de l'ACE</li>
+	  <li> Les médias et le folklore</li>
+	  <li> M6 : La réponse des cercles non-folkloriques</li>
+	  <li> Le patrimoine de l’UL.B. </li>
+	  <li> Le Sida a-t-il une fin ? </li>
+	  <li> Le LHC, Large Hadron Collissionner</li>
+	  <li> Psycampus</li>
+	  <li> Musique de l’ULB : Five Easy Days</li>
+	  <li> Toone. du pure folklore bruxellois </li>
+	  <li> La culture à l’U.L.B.</li>
+	  <li> Sacrilège ! La religion satirisée</li>
+	  <li> U.L.B. S’expose</li>
+	  <li> Les midis musicaux</li>
+	  <li> Exposition Jack Freak Pictures</li>
+	  <li> Les midis de la poésie </li>
+	  <li> préhistoire au cinéma </li>
+	  <li> La Selection de Lise </li>
+	  <li> “48H pour convaincre” </li>
+	  <div align="center">
+		<?php echo HTML::file_anchor('public/heresie/heresie2.pdf', 'Télécharger', array('class' => 'btn')); ?>
+	  </div>
+	</ul>
+  </div>
+  <div class="span5">
+	<?php echo HTML::file_anchor('public/heresie/heresie2.pdf', HTML::image('public/images/heresie2.jpg', array('alt' => 'Hérésie 2'))); ?>
+  </div>
+</div>
+<div class="row">
+  <div class="span11">
+	<h3>Hérésie n°1  <small>Novembre 2010</small></h3>
+	<h5>Sommaire :</h5>
+	<ul>
+	  <li>Présentation de l’A.C.E.</li>
+	  <li>La Saint-Verhaegen et l’engagement</li>
+	  <li>Le festival belge de la chanson estudiantine 2010</li>
+	  <li>Pierre de Maret, Portrait du Pro-Recteur</li>
+	  <li>Présentation de la Coordination Environnementale</li>
+	  <li>L’anniversaire du Cercle d’Histoire</li>
+	  <li>Musique de l’ULB : THE MERIDIANS</li>
+	  <li>Francis Alÿs, A story of Deception</li>
+	  <li>Le projet NEEM : un forage révélateur</li>
+	  <li>Sex & The University (Aimer à l’ULB)</li>
+	  <li>30 ans de Radios libres en Belgique</li>
+	  <li>Histoire de Bruxelles : Le Meyboom</li>
+	  <li>Le Cercle des Romanes</li>
+	  <li>Le cimetière d’Ixelles</li>
+	  <li>Le Centre de recherches archéologiques de l’ULB</li>
+	  <li>Quel avenir pour le folklore ?</li>
+	  <li>Deuxième partie du Cycle Maxime Steinberg</li>
+	  <li>Les Ateliers d’ULB CULTURE</li>
+	  <li>L’Agenda Culturel & La Selection de Lise</li>
+	  <div align="center">
+		<?php echo HTML::file_anchor('public/heresie/heresie1.pdf', 'Télécharger', array('class' => 'btn')); ?>
+	  </div>
+	</ul>
+  </div>
+  <div class="span5">
+	<?php echo HTML::file_anchor('public/heresie/heresie1.pdf', HTML::image('public/images/heresie1.jpg', array('alt' => 'Hérésie 1'))); ?>
+  </div>
+</div>
