8373096012624d5bbfe527927c4b6bf3384a7d4d
Laurent <lcontzen@gmail.com>
2012-02-21 13:42:29 +0100

    New controler template_website

---
+++ application/classes/controller/template/website.php
@@ -0,0 +1,32 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+class Controller_Template_Website extends Controller_Template {
+  public $template = 'template/website';
+
+  public function before() {
+	parent::before();
+	if ($this->auto_render) {
+	  $this->template->title = '';
+	  $this->template->content = '';
+	  $this->template->styles = array();
+	  $this->template->scripts = array();
+	}
+  }
+
+  public function after() {
+	if ($this->auto_render) {
+	  $styles = array(
+					  'public/theme/css/style.css' => 'screen, projection',
+					  'public/theme/css/prettify.css' => 'screen, projection',
+					  );
+	  $scripts = array(
+					   'public/theme/js/jquery-1.5.2.min.js',
+					   'public/theme/js/prettify.js',
+					   'public/theme/js/application.js',
+					   'public/theme/js/bootstrap-dropdown.js',
+					   );
+	  $this->template->styles = array_merge($this->template->styles, $styles);
+	  $this->template->scripts = array_merge($this->template->scripts, $scripts);
+	}
+	parent::after();
+  }
+}
