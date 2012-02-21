51434f2c4897fca44ee897a7bd3a1aed3655635b
Laurent <lcontzen@gmail.com>
2012-02-21 15:19:16 +0100

    New messages file

---
+++ application/message/aceulb.php
@@ -0,0 +1,6 @@
+<?php  defined('SYSPATH') or die('No direct script access.');
+return array(
+			 'permission' => 'You don\'t have the permission',
+			 'wrong' => 'Wrong username or password',
+			 );
+?>
