c1bdcf21a7328167f816918a09d189f4803e4738
Laurent <lcontzen@gmail.com>
2012-02-21 15:06:42 +0100

    New config aceulb

---
+++ application/config/aceulb.php
@@ -0,0 +1,14 @@
+<?php defined ('SYSPATH') or die('No direct access allowed');
+
+return array(
+			 'myconfig1' => 'fjdsjkfdpolktrew',
+			 'playlists' => array(
+								  1478363 => 10171,
+								  22857234 => 10171,
+								  386 => 10171,
+								  722 => 10171,
+								  893 => 10171,
+								  237 => 10171,
+								  ),
+			 );
+?>
