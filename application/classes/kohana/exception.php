2afc9efbb3d12d7af13dd0579551430cfca7d1ba
Laurent <lcontzen@gmail.com>
2012-02-21 15:51:19 +0100

    Fixed multiple errors in exception.php

--- application/classes/kohana/exception.php
+++ application/classes/kohana/exception.php
@@ -4,27 +4,36 @@ class Kohana_Exception extends Kohana_Kohana_Exception {
 	if (Kohana::DEVELOPMENT === Kohana::$environment) {
 	  parent::handler($e);
 	}
-  }
-  else {
-	try {
-	  Kohana::$log->add(Log::ERROR, parent::text($e));
-	  $attributes = array(
-						  'controller' => 'error',
-						  'action' => 500,
-						  'message' => rawurlencode($e->getMessage())
-						  );
-	  if ($e instanceof HTTP_Exception) {
-		$attributes['action'] = $e->getCode();
+	else {
+	  try {
+		Kohana::$log->add(Log::ERROR, parent::text($e));
+		
+		$attributes = array(
+							'controller' => 'error',
+							'action'  => 500,
+							'message' => rawurlencode($e->getMessage())
+							);
+		
+		if ($e instanceof HTTP_Exception) {
+		  $attributes['action'] = $e->getCode();
+		}
+		
+		// Error sub-request.
+		echo Request::factory(Route::get('error')->uri($attributes))
+		  ->execute()
+		  ->send_headers()
+		  ->body();
 	  }
-	  echo Request::factory(Route::get('error')->uri($attributes))
-		->execute()
-		->send_headers()
-		->body();
-	}
-	catch (Exception $e) {
-	  ob_get_level() and ob_clean();
-	  echo parent::text($e);
-	  exit(1);
+	  catch (Exception $e) {
+		// Clean the output buffer if one exists
+		ob_get_level() and ob_clean();
+		
+		// Display the exception text
+		echo parent::text($e);
+		
+		// Exit with an error status
+		exit(1);
+	  }
 	}
   }
 }
