755724b72e584300081a3c223eefeb12437295fa
Laurent <lcontzen@gmail.com>
2012-02-22 10:35:52 +0100

    Fixed bugs in user controller

--- application/classes/controller/user.php
+++ application/classes/controller/user.php
@@ -1,7 +1,6 @@
 <?php defined('SYSPATH') or die('No direct access allowed.');
 
 class Controller_User extends Controller_Template_Aceulb {
-
   public function action_index() {
 	$view = View::factory('user/index');
 	$this->template->set('content', $view);
@@ -11,24 +10,23 @@ class Controller_User extends Controller_Template_Aceulb {
 	$view = View::factory('user/create')
 	  ->set('values', $_POST)
 	  ->bind('errors', $errors);
-
 	if ($_POST) {
 	  $user = ORM::factory('user')
 		->values($_POST, array('username', 'password'));
-
 	  $external_values = array(
 							   'password' => Arr::get($_POST, 'password'),
 							   ) + Arr::get($_POST, '_external', array());
 	  $extra = Validation::factory($external_values)
 		->rule('password_confirm', 'matches', array(':validation', ':field', 'password'));
-
 	  try {
 		$user->save($extra);
-		$this->request->redirect('user/'.$user->id);
+		/* $this->request->redirect('user/'.$user->id); */
+		$this->request->redirect('user/index');
 	  } catch (ORM_Validation_Exception $e) {
 		$errors = $e->errors('models');
 	  }
 	}
-	$this->response->body($view);
+	$this->template->set('content', $view);
+	/* $this->response->body($view); */
   }
 }
