8a91c7ef325cc6f4a1ae057293aef0bc14166922
Laurent <lcontzen@gmail.com>
2012-02-22 12:34:58 +0100

    Added slug field rules in article model

--- application/classes/model/article.php
+++ application/classes/model/article.php
@@ -3,6 +3,10 @@
 class Model_Article extends ORM {
   public function rules() {
 	return array(
+				 'slug' => array(
+								 array('not_empty'),
+								 array('max_length', array(':value', 30)),
+								 ),
 				 'title' => array(
 								  array('not_empty'),
 								  array('max_length', array(':value', 50)),
