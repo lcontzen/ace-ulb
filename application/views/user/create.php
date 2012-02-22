cf7f6a51591ed4d05a10be248549d836c2c7d896
Laurent <lcontzen@gmail.com>
2012-02-22 10:37:46 +0100

    Changed <?= to <?php in user/create view

--- application/views/user/create.php
+++ application/views/user/create.php
@@ -1,15 +1,15 @@
-<form action="<?= URL::site('/members'); ?>" method="post" accept-charset="utf-8">
+<form action="<?php URL::site('/user'); ?>" method="post" accept-charset="utf-8">
     <label for="username">Username:</label>
-    <input id="username" type="text" name="username" value="<?= Arr::get($values, 'username'); ?>" />
-    <label for="username" class="error"><?= Arr::get($errors, 'username'); ?>
+    <input id="username" type="text" name="username" value="<?php Arr::get($values, 'username'); ?>" />
+    <label for="username" class="error"><?php Arr::get($errors, 'username'); ?>
  
     <label for="password">Password:</label>
-    <input id="password" type="password" name="password" value="<?= Arr::get($values, 'password'); ?>" />
-    <label for="password" class="error"><?= Arr::get($errors, 'password'); ?>
+    <input id="password" type="password" name="password" value="<?php Arr::get($values, 'password'); ?>" />
+    <label for="password" class="error"><?php Arr::get($errors, 'password'); ?>
  
     <label for="password_confirm">Repeat Password:</label>
-    <input id="password_confirm" type="password" name="_external[password_confirm]" value="<?= Arr::path($values, '_external.password_confirm'); ?>" />
-    <label for="password_confirm" class="error"><?= Arr::path($errors, '_external.password_confirm'); ?>
+    <input id="password_confirm" type="password" name="_external[password_confirm]" value="<?php Arr::path($values, '_external.password_confirm'); ?>" />
+    <label for="password_confirm" class="error"><?php Arr::path($errors, '_external.password_confirm'); ?>
  
     <button type="submit">Create</button>
 </form>
