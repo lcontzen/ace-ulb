100c950f2000405a499459c8af40500c67754fb5
Laurent <lcontzen@gmail.com>
2012-02-22 11:45:17 +0100

    New article model

---
+++ application/classes/model/article.php
@@ -0,0 +1,20 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+class Model_Article extends ORM {
+  public function rules() {
+	return array(
+				 'title' => array(
+								  array('not_empty'),
+								  array('max_length', array(':value', 50)),
+								  ),
+				 'body' => array(
+								 array('not_empty'),
+								 array('max_length', array(':value', 2000)),
+								 ),
+				 'author_id' => array(
+									  array('not_empty'),
+									  array('max_length', array(':value', 11)),
+									  ),
+				 );
+  }
+}
