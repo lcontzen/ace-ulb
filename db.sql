13801c7aea53612707b92f589331928d54ce5f5b
Laurent <lcontzen@gmail.com>
2012-02-22 11:37:42 +0100

    Added 5 tables in db.sql

--- db.sql
+++ db.sql
@@ -28,6 +28,16 @@ CREATE TABLE IF NOT EXISTS `cercles` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+CREATE TABLE IF NOT EXISTS `ordres` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,  
+  `description` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `logo_link` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `cercle_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`cercle_id`) REFERENCES cercles(`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
 CREATE TABLE IF NOT EXISTS `articles` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
@@ -44,3 +54,37 @@ CREATE TABLE IF NOT EXISTS `news` (
   FOREIGN KEY (`article_id`) REFERENCES articles(`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+CREATE TABLE IF NOT EXISTS `pages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `article_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`article_id`) REFERENCES articles(`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `comitees_members` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `picture_link` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `gsm_number` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `mail_address` varchar(200)  CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `person_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`person_id`) REFERENCES persons(`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `functions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cercle_id` int(11) NOT NULL,
+  `comitees_member_id` int(11) NOT NULL,
+  `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`comitees_member_id`) REFERENCES comitees_members(`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `vlecks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ordre_id` int(11) NOT NULL,
+  `person_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`ordre_id`) REFERENCES ordres(`id`),
+  FOREIGN KEY (`person_id`) REFERENCES persons(`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
