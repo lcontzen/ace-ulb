ffdf4c566c08f937f2d12da2e7dd99a3cb0c7ad3
Laurent <lcontzen@gmail.com>
2012-02-26 12:35:22 +0100

    Cleaned form presentation in admin/adduser view

--- application/views/admin/adduser.php
+++ application/views/admin/adduser.php
@@ -1,70 +1,91 @@
-<h2>Add a New User</h2>
+<div class="page-header">
+  <h1>Add a New User</h1>  
+</div>
 <?php if ($message) : ?>
-    <h3 class="message">
-        <?php echo $message; ?>
-    </h3>
+<div><strong><em><?php echo $message; ?></em></strong></div>
 <?php endif; ?>
- 
+
 <?php echo Form::open('admin/adduser'); ?>
-
-<?php echo Form::label('first_name', 'Prénom'); ?>
-<?php echo Form::input('first_name', HTML::chars(Arr::get($_POST, 'first_name'))); ?>
-<div class="error">
-    <?php if ($errors) :
-   	echo Arr::get($errors, 'first_name');
-	endif; ?>
-</div>
-
-<?php echo Form::label('last_name', 'Nom'); ?>
-<?php echo Form::input('last_name', HTML::chars(Arr::get($_POST, 'last_name'))); ?>
-<div class="error">
-    <?php if ($errors) :
-   	echo Arr::get($errors, 'last_name');
-	endif; ?>
-</div>
-
-<?php echo Form::label('username', 'Username'); ?>
-<?php echo Form::input('username', HTML::chars(Arr::get($_POST, 'username'))); ?>
-<div class="error">
-    <?php if ($errors) :
-   	echo Arr::get($errors, 'username');
-	endif; ?>
-</div>
- 
-<?php echo Form::label('status', 'Status'); ?>
-<?php echo Form::input('status', HTML::chars(Arr::get($_POST, 'status'))); ?>
-<div class="error">
-    <?php
-	  if ($errors) :
-		echo Arr::get($errors, 'status');
-	endif; ?>
-</div>
-
-<?php echo Form::label('cercle_id', 'Cercle Id'); ?>
-<?php echo Form::input('cercle_id', HTML::chars(Arr::get($_POST, 'cercle_id'))); ?>
-<div class="error">
-    <?php
-	  if ($errors) :
-		echo Arr::get($errors, 'cercle_id');
-	endif; ?>
-</div>
-
-<?php echo Form::label('password', 'Password'); ?>
-<?php echo Form::password('password'); ?>
-<div class="error">
-    <?php if ($errors) :
-	  echo Arr::path($errors, '_external.password');
-	endif; ?>
-</div>
- 
-<?php echo Form::label('password_confirm', 'Confirm Password'); ?>
-<?php echo Form::password('password_confirm'); ?>
-<div class="error">
-    <?php if ($errors) :
-	  echo Arr::path($errors, '_external.password_confirm');
-	endif; ?>
-</div>
- 
-<?php echo Form::submit('adduser', 'Add User'); ?>
+<fieldset>
+  <div class="clearfix">
+	<?php echo Form::label('first_name', 'Prénom'); ?>
+	<div class="input">
+	  <?php echo Form::input('first_name', HTML::chars(Arr::get($_POST, 'first_name')), array('class' => 'xlarge', 'size' => '30', 'type' => 'text')); ?>
+	  <span>
+		<?php if ($errors) :
+   		  echo Arr::get($errors, 'first_name');
+		  endif; ?>
+	  </span>
+	</div>
+  </div>
+  <div class="clearfix">
+	<?php echo Form::label('last_name', 'Nom'); ?>
+	<div class="input">
+	  <?php echo Form::input('last_name', HTML::chars(Arr::get($_POST, 'last_name')), array('class' => 'xlarge', 'size' => '30', 'type' => 'text')); ?>
+	  <span class="error">
+		<?php if ($errors) :
+			echo Arr::get($errors, 'last_name');
+		  endif; ?>
+	  </span>
+	</div>
+  </div>
+  <div class="clearfix">
+	<?php echo Form::label('username', 'Username'); ?>
+	<div class="input">
+	  <?php echo Form::input('username', HTML::chars(Arr::get($_POST, 'username')), array('class' => 'xlarge', 'size' => '30', 'type' => 'text')); ?>
+	  <span class="error">
+		<?php if ($errors) :
+			echo Arr::get($errors, 'username');
+		  endif; ?>
+	  </span>  
+	</div>
+  </div>
+  <div class="clearfix">
+	<?php echo Form::label('status', 'Status'); ?>
+	<div class="input">
+	  <?php echo Form::input('status', HTML::chars(Arr::get($_POST, 'status')), array('class' => 'xlarge', 'size' => '30', 'type' => 'text')); ?>
+	  <span class="error">
+		<?php if ($errors) :
+			echo Arr::get($errors, 'status');
+		  endif; ?>
+	  </span>
+	</div>  
+  </div>
+  <div class="clearfix">
+	<?php echo Form::label('cercle_id', 'Cercle Id'); ?>
+	<div class="input">
+	  <?php echo Form::input('cercle_id', HTML::chars(Arr::get($_POST, 'cercle_id')), array('class' => 'xlarge', 'size' => '30', 'type' => 'text')); ?>
+	  <span class="error">
+		<?php if ($errors) :
+			echo Arr::get($errors, 'cercle_id');
+		  endif; ?>
+	  </span>
+	</div>
+  </div>
+  <div class="clearfix">
+	<?php echo Form::label('password', 'Password'); ?>
+	<div class="input">
+	  <?php echo Form::password('password', NULL, array('class' => 'xlarge', 'size' => '30')); ?>
+	  <span class="error">
+		<?php if ($errors) :
+			echo Arr::path($errors, '_external.password');
+		  endif; ?>
+	  </span>
+	</div>
+  </div>
+  <div class="clearfix">
+	<?php echo Form::label('password_confirm', 'Confirm Password'); ?>
+	<div class="input">
+			<?php echo Form::password('password_confirm', NULL, array('class' => 'xlarge', 'size' => '30')); ?>
+	  <span class="error">
+		<?php if ($errors) :
+			echo Arr::path($errors, '_external.password_confirm');
+		  endif; ?>
+	  </span>
+	</div>
+  </div>
+  <div class="actions">
+	<?php echo Form::submit('adduser', 'Add User'); ?>
+  </div>
+</fieldset>
 <?php echo Form::close(); ?>
- 
