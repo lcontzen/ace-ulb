12491149e695890ab0092feb70903ae87dcb695e
Laurent <lcontzen@gmail.com>
2012-02-26 11:06:38 +0100

    New person errors messages

---
+++ application/messages/models/person.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+			 'first_name' => array(
+								   'not_empty' => 'You must provide a First Name',
+								   ),
+			 'last_name' => array(
+								  'not_empty' => 'You must provide a last name',
+								  ),
+			 );
