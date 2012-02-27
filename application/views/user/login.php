9de7f8f9bc4fe20cb30711ca783a69a937b4241c
Laurent <lcontzen@gmail.com>
2012-02-27 11:02:07 +0100

    Recreated user/login view to be visually coherent with the rest

--- application/views/user/login.php
+++ application/views/user/login.php
@@ -1,24 +1,28 @@
-<h2>Login</h2>
+<div class="page-header">
+  <h1>Login</h1>
+</div>
 <?php if ($message) : ?>
-    <h3 class="message">
-        <?php echo $message; ?>
-    </h3>
+<h3 class="message">
+  <?php echo $message; ?>
+</h3>
 <?php endif; ?>
 
 <?php echo Form::open('user/login'); ?>
- 
-<?php echo Form::label('username', 'Username'); ?>
-<?php echo Form::input('username', HTML::chars(Arr::get($_POST, 'username'))); ?>
- <br />
-<?php echo Form::label('password', 'Password'); ?>
-<?php echo Form::password('password'); ?>
- <br />
-<?php echo Form::label('remember', 'Remember Me'); ?>
-<?php echo Form::checkbox('remember'); ?>
- <br />
-<p>(Remember Me keeps you logged in for 2 weeks)</p>
- <br />
-<?php echo Form::submit('login', 'Login'); ?>
+<fieldset>
+  <div class="clearfix">
+	<?php echo Form::label('username', 'Username'); ?>
+	<div class="input">
+	  <?php echo Form::input('username', HTML::chars(Arr::get($_POST, 'username')), array('class' => 'xlarge', 'size' => '30', 'type' => 'text')); ?>
+	</div>
+  </div>
+  <div class="clearfix">
+	<?php echo Form::label('password', 'Password'); ?>
+	<div class="input">
+	  <?php echo Form::password('password', HTML::chars(Arr::get($_POST, 'password')), array('class' => 'xlarge', 'size' => '30')); ?>
+	</div>
+  </div>
+  <div class="actions">
+	<?php echo Form::submit('login', 'Login'); ?>
+  </div>  
+</fieldset>
 <?php echo Form::close(); ?>
- <br />
-<p>Or <?php echo HTML::anchor('user/create', 'create a new account'); ?></p>
