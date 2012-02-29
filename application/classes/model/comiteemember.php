9f1b1dd055a280ceb54037a60f2c92336c0f6347
Laurent <lcontzen@gmail.com>
2012-02-29 11:01:36 +0100

    New model for comiteemembers table

---
+++ application/classes/model/comiteemember.php
@@ -0,0 +1,37 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+class Model_ComiteeMember extends ORM {  
+  public function rules() {
+	return array(
+				 'picture_link' => array(
+										 array('not_empty'),
+										 array('max_length', array(':value', 200)),
+										 ),
+				 'gsm_number' => array(
+										 array('not_empty'),
+										 array('max_length', array(':value', 64)),
+										 ),
+				 'mail_address' => array(
+										 array('not_empty'),
+										 array('max_length', array(':value', 200)),
+										 ),
+				 'person_id' => array(
+										 array('not_empty'),
+										 array('max_length', array(':value', 11)),
+										 ),
+				 );
+  }
+
+  public function labels() {
+	return array(
+				 'picture_link' => 'picture_link',
+				 'gsm_number' => 'gsm_number',
+				 'mail_address' => 'mail_address',
+				 'person_id' => 'person_id',
+				 );
+  }
+
+  public function create_comitees_member($values, $expected) {
+	return $this->values($values, $expected)->create();
+  }
+}
