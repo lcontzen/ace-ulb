ba160f34a6107f5a85aa5e1dbacc09b732fb95c1
Laurent <lcontzen@gmail.com>
2012-03-06 12:28:16 +0100

    Checks if the given function exists in the given cercle before inserting it

--- application/classes/controller/admin.php
+++ application/classes/controller/admin.php
@@ -7,6 +7,14 @@ class Controller_Admin extends Controller_Template_Aceulb {
 	$this->template->set('content', $view);
   }
 
+  private function _function_exists($cercle_id, $function) {
+	return ORM::factory('comiteemember')
+	  ->where('cercle_id', '=', $cercle_id)
+	  ->and_where('function', '=', $function)
+	  ->find()
+	  ->loaded();
+  } 
+  
   public function action_addcomiteemember() {
 	$this->check_admin_status();
 	$view = View::factory('admin/addcomiteemember');
@@ -18,25 +26,27 @@ class Controller_Admin extends Controller_Template_Aceulb {
 	}
 	if (HTTP_Request::POST == $this->request->method()) {
 	  try {
-		/* $person = ORM::factory('person'); */
-		/* $person->create_person($this->request->post(), array( */
-		/* 													 'first_name', */
-		/* 													 'last_name')); */
-		$comitee_member = ORM::factory('comiteemember');
-		$values = $this->request->post();
-		/* $values['person_id'] = $person->id; */
-		$comitee_member->create_comitees_member($values, array(
-															   'cercle_id',
-															   'function',
-															   'first_name',
-															   'last_name',
-															   'picture_link',
-															   'gsm_number',
-															   'mail_address',
-															   ));
-		$_POST = array();
-		$message = "Comitee member added!";
-		$view->set('message', $message);
+		$cercle_id = $this->request->post('cercle_id');
+		$function = $this->request->post('function');
+		if (!$this->_function_exists($cercle_id, $function)) {
+		  $comitee_member = ORM::factory('comiteemember');
+		  $values = $this->request->post();
+		  $comitee_member->create_comitees_member($values, array(
+																 'cercle_id',
+																 'function',
+																 'first_name',
+																 'last_name',
+																 'picture_link',
+																 'gsm_number',
+																 'mail_address',
+																 ));
+		  $_POST = array();
+		  $message = "Comitee member added!";
+		  $view->set('message', $message);
+		} else {
+		  $message = "Ce poste existe déjà";
+		  $view->set('message', $message);
+		}
 	  } catch (ORM_Validation_Exception $e) {
 		$message = 'There were errors, please see form below.';
 		$view->set('message', $message);
@@ -59,25 +69,28 @@ class Controller_Admin extends Controller_Template_Aceulb {
 	}
 	if (HTTP_Request::POST == $this->request->method()) {
 	  try {
+		$count = 0;
 		$functions = $this->request->post('functions');
 		$functions_list = explode(', ', $functions);
 		foreach ($functions_list as $function_item) {
-		  $values = array();
-		  $values['function'] = $function_item;
-		  $values['cercle_id'] = $this->request->post('cercle_id');
-		  $comitee_member = ORM::factory('comiteemember');
-		  $comitee_member->create_comitees_member($values, array(
-																 'cercle_id',
-																 'function',
-																 'first_name',
-																 'last_name',
-																 'picture_link',
-																 'gsm_number',
-																 'mail_address',
-																 ));
+		  if (!$this->_function_exists($this->request->post('cercle_id'), $function_item)) {
+			$values = array();
+			$values['function'] = $function_item;
+			$values['cercle_id'] = $this->request->post('cercle_id');
+			$comitee_member = ORM::factory('comiteemember');
+			$comitee_member->create_comitees_member($values, array(
+																   'cercle_id',
+																   'function',
+																   'first_name',
+																   'last_name',
+																   'picture_link',
+																   'gsm_number',
+																   'mail_address',
+																   ));
+			$count = $count + 1;
+		  }
 		}
 		$_POST = array();
-		$count = count($functions_list);
 		$message = "You have added {$count} functions to the database";
 		$view->set('message', $message);
 	  } catch (ORM_Validation_Exception $e) {
