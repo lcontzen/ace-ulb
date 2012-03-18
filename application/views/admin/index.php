8fdd4d03c1536af336bdd13f1fa3929260987876
Laurent <lcontzen@gmail.com>
2012-03-18 12:36:22 +0100

    Added new links in admin/index view

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -5,4 +5,6 @@
   <li><?php echo HTML::anchor('user/add', 'Add new user'); ?></li>
   <li><?php echo HTML::anchor('article/addnews', 'Create new news'); ?></li>
   <li><?php echo HTML::anchor('article/listnews', 'Manage news'); ?></li>
+  <li><?php echo HTML::anchor('admin/addflist', 'Add functions list'); ?></li>
+  <li><?php echo HTML::anchor('admin/addcomiteemember', 'Add Comitee Member'); ?></li>
 </ul>
