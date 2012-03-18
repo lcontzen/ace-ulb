d0117e941bfda6e33e5e666e456f7d1a52b21dec
Laurent <lcontzen@gmail.com>
2012-03-18 16:05:58 +0100

    Added html2pdf module

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -111,6 +111,7 @@ Kohana::modules(array(
 	'orm'        => MODPATH.'orm',        // Object Relationship Mapping
 	// 'unittest'   => MODPATH.'unittest',   // Unit testing
 	// 'userguide'  => MODPATH.'userguide',  // User guide and API documentation
+					  'html2pdf' => MODPATH.'html2pdf',
 	));
 
 /**
