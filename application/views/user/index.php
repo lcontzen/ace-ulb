bb50a2e23060bf582e2ef499ef74bb3d6ed7c450
Laurent <lcontzen@gmail.com>
2012-03-18 12:20:42 +0100

    Slightly modified user/index view

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -1,3 +1,7 @@
 <div class="page-header">
-   <h1>Welcome, <?php echo $user->username; ?></h1>
+   <h1>Bienvenue, <?php echo $user->username; ?></h1>
 </div>
+
+<ul>
+  <li><?php echo HTML::anchor('comitee/manage', "Gérer ma liste ACE"); ?></li>
+</ul>
