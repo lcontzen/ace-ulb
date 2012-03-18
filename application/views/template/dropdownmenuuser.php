2056c47ea57011d37be376f9f328eb5a85f09ac1
Laurent <lcontzen@gmail.com>
2012-03-18 12:22:31 +0100

    Slightly modified template/dropdownmenuuser.php view

--- application/views/template/dropdownmenuuser.php
+++ application/views/template/dropdownmenuuser.php
@@ -2,7 +2,7 @@
 			  <li class="dropdown">
 				<?php echo HTML::anchor('#', Auth::instance()->get_user()->username, array('class' => 'dropdown-toggle')); ?>
 				<ul class="dropdown-menu">
-                  <li><a href="#">Todo</a></li>
+                  <li><a href="user/index">Control Panel</a></li>
                   <li class="divider"></li>
 				  <li><?php echo HTML::anchor('user/logout', 'Logout'); ?></li>
 				</ul>
