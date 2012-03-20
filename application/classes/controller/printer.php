3244a86276ce02b26b004465eddfce6ec91c9b30
Laurent <lcontzen@gmail.com>
2012-03-20 11:54:54 +0100

    New methods for pdf generation

--- application/classes/controller/printer.php
+++ application/classes/controller/printer.php
@@ -29,4 +29,35 @@ class Controller_Printer extends Controller_Template_Printing {
 	$view->render();
 	$this->request->body($view);
   }
+
+  public function action_pdflists() {
+	$config = array(
+					'author'   => 'Laurent C',
+					'title'    => 'Listes ACE 2011-2012',
+					'subject'  => 'Your subject',
+					'name'     => Text::random().'.pdf', // name file pdf
+					);
+	$view = View_PDF::factory('printer/lists', $config);
+	$cercles = array();
+	$cercles_temp = ORM::factory('cercle')
+	  ->order_by('id')
+	  /* ->limit(15) */
+	  ->find_all();
+	$count = 0;
+	foreach ($cercles_temp as $cercle) {
+	  $cercles[$count]['name'] = $cercle->name;
+	  $comitee_members =  ORM::factory('comiteemember')
+	  ->where('cercle_id', '=', $cercle->id)
+	  ->order_by('id')
+	  ->find_all();
+	  $cercles[$count]['comitee_members'] = $comitee_members;
+	  $count = $count + 1;
+	}
+	$view->set('count', $count);
+	$view->set('cercles', $cercles);
+
+	$view->render();
+	$this->request->body($view);
+	
+  }
 }
