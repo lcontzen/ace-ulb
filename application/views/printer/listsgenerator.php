690e9bb8baa4513dac064200844ca418e449b3fc
Laurent <lcontzen@gmail.com>
2012-03-20 11:24:56 +0100

    Various modifications in test page

--- application/views/printer/listsgenerator.php
+++ application/views/printer/listsgenerator.php
@@ -1,6 +1,10 @@
-<div class="page-header">
-  <h1>Test</h1>
-</div>
+<!-- <div class="page-header"> -->
+<!--   <h1>Test</h1> -->
+<!-- </div> -->
+
+<!-- <div class="frontcover"> -->
+<!--   <h2>Listes ACE 2011-2012</h2> -->
+<!-- </div> -->
 
 <!-- start slipsum code -->
 <div class="chapter" id="html-h-1">
@@ -14,7 +18,7 @@
 
 
 <div class="chapter" id="html-h-2">
-  <h1 style="counter-reset: page 1">Chapter 2</h1>
+  <h1>Chapter 2</h1>
 
 <p>Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass. </p>
 
@@ -41,3 +45,4 @@
 <p>Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in American crime. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like they're actually proud of that shit.  </p>
 
 <p>You think water moves fast? You should see ice. It moves like it has a mind. Like it knows it killed the world once and got a taste for murder. After the avalanche, it took us a week to climb out. Now, I don't know exactly when we turned on each other, but I know that seven of us survived the slide... and only five made it out. Now we took an oath, that I'm breaking now. We said we'd say it was the snow that killed the other two, but it wasn't. Nature is lethal but it doesn't hold a candle to man. </p>
+</div>
