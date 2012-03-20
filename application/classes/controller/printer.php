ec785b6482b745ff49abdce9aa5bdfb45f55719c
Laurent <lcontzen@gmail.com>
2012-03-20 11:23:21 +0100

    New methods generating pdfs

--- application/classes/controller/printer.php
+++ application/classes/controller/printer.php
@@ -5,4 +5,28 @@ class Controller_Printer extends Controller_Template_Printing {
 	$view = View::factory('printer/listsgenerator');
 	$this->template->set('content', $view);
   }
+
+  public function action_testpdf() {
+	$config = array(
+					'author'   => 'Laurent C',
+					'title'    => 'Listes ACE 2011-2012',
+					'subject'  => 'Your subject',
+					'name'     => Text::random().'.pdf', // name file pdf
+					);
+	$view = View_PDF::factory('printer/listsgenerator', $config);
+	$view->render();
+	$this->request->body($view);
+  }
+
+  public function action_pdfpages() {
+	$config = array(
+					'author'   => 'Laurent C',
+					'title'    => 'Listes ACE 2011-2012',
+					'subject'  => 'Your subject',
+					'name'     => Text::random().'.pdf', // name file pdf
+					);
+	$view = View_PDF::factory('printer/pdfpages', $config);
+	$view->render();
+	$this->request->body($view);
+  }
 }
