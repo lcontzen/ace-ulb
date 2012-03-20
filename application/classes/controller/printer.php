8c5f2d414ec32eb0f8063ed295986417b3253754
Laurent <lcontzen@gmail.com>
2012-03-20 12:44:17 +0100

    Cleaned up useless code for pdf generation tries from printer controller and views

--- application/classes/controller/printer.php
+++ application/classes/controller/printer.php
@@ -1,35 +1,6 @@
 <?php defined('SYSPATH') or die('No direct access allowed.');
 
 class Controller_Printer extends Controller_Template_Printing {
-  public function action_test() {
-	$view = View::factory('printer/listsgenerator');
-	$this->template->set('content', $view);
-  }
-
-  public function action_testpdf() {
-	$config = array(
-					'author'   => 'Laurent C',
-					'title'    => 'Listes ACE 2011-2012',
-					'subject'  => 'Your subject',
-					'name'     => Text::random().'.pdf', // name file pdf
-					);
-	$view = View_PDF::factory('printer/listsgenerator', $config);
-	$view->render();
-	$this->request->body($view);
-  }
-
-  public function action_pdfpages() {
-	$config = array(
-					'author'   => 'Laurent C',
-					'title'    => 'Listes ACE 2011-2012',
-					'subject'  => 'Your subject',
-					'name'     => Text::random().'.pdf', // name file pdf
-					);
-	$view = View_PDF::factory('printer/pdfpages', $config);
-	$view->render();
-	$this->request->body($view);
-  }
-
   public function action_pdflists() {
 	$config = array(
 					'author'   => 'Laurent C',
@@ -41,7 +12,6 @@ class Controller_Printer extends Controller_Template_Printing {
 	$cercles = array();
 	$cercles_temp = ORM::factory('cercle')
 	  ->order_by('id')
-	  /* ->limit(15) */
 	  ->find_all();
 	$count = 0;
 	foreach ($cercles_temp as $cercle) {
