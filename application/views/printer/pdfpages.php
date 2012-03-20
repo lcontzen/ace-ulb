4a06bb368fc614c7c62574f1eb7e303a60cd1eff
Laurent <lcontzen@gmail.com>
2012-03-20 11:22:52 +0100

    New view using html2pdf's pages tag

---
+++ application/views/printer/pdfpages.php
@@ -0,0 +1,24 @@
+<page backtop="14mm" backbottom="14mm" backleft="10mm" backright="10mm" style="font-size: 12pt">
+	<page_header>
+		<table class="page_header">
+			<tr>
+				<td style="width: 100%; text-align: left">
+					Example of using bookmarks
+				</td>
+			</tr>
+		</table>
+	</page_header>
+	<page_footer>
+		<table class="page_footer">
+			<tr>
+				<td style="width: 100%; text-align: right">
+					page [[page_cu]]/[[page_nb]]
+				</td>
+			</tr>
+		</table>
+	</page_footer>
+	<bookmark title="Chapter 1" level="0" ></bookmark><h1>Chapter 1</h1>
+	<div class="standard">
+		Contents of Chapter 1
+	</div>
+</page>
