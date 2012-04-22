e51fcf00f08b41aa7dfe0c8846f5b0eac6517f49
Laurent Contzen <lcontzen@gmail.com>
2012-04-22 11:25:02 +0200

    Added link to article/addpage in admin/index

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -5,10 +5,10 @@
   <li><?php echo HTML::anchor('user/add', 'Add new user'); ?></li>
   <li><?php echo HTML::anchor('article/addnews', 'Create new news'); ?></li>
   <li><?php echo HTML::anchor('article/listnews', 'Manage news'); ?></li>
+  <li><?php echo HTML::anchor('article/addpage', 'Create new page'); ?></li>
   <li><?php echo HTML::anchor('admin/addflist', 'Add functions list'); ?></li>
   <li><?php echo HTML::anchor('admin/addcomiteemember', 'Add Comitee Member'); ?></li>
   <li><?php echo HTML::anchor('admin/managelistchoice', 'Manage a list'); ?></li>
   <li><?php echo HTML::anchor('printer/pdflists', 'Generate lists pdf'); ?></li>
-<li><?php echo HTML::file_anchor('public/listes/ListesACE.pdf', 'Get lists pdf'); ?></li>
-  
+  <li><?php echo HTML::file_anchor('public/listes/ListesACE.pdf', 'Get lists pdf'); ?></li>
 </ul>
