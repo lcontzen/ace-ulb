50a7ec2252123436af1465277b67e367a4192b16
Laurent Contzen <lcontzen@gmail.com>
2012-06-09 12:12:58 +0200

    Modified tds.php

--- application/views/page/tds.php
+++ application/views/page/tds.php
@@ -2,104 +2,123 @@
   <h1>Agenda des TDs</h1>
 </div>
 <div class="row">
-  <div class="span12">
-	<h2>Février</h2>
-	<table class="table table-bordered table-striped">
+  <div class="span14">
+	<h2>Juin</h2>
+	<table class="table table-brodered table-striped">
 	  <tbody>
 		<tr>
-		  <td> <b>Lundi 06/02</b> <br /> Kiné </td>
-		  <td> <b>Mardi 07/02</b> <br /> Pharma </td>
-		  <td> <b>Mercredi 08/02</b> <br /> CPS-Philo </td>
-		  <td> <b>Jeudi 09/02</b> <br /> CdH </td>
+		  <td><b>Lundi <br /> 18/06</b> <br /> / </td>
+		  <td><b>Mardi <br /> 19/06</b> <br /> / </td>
+		  <td><b>Mercredi <br /> 20/06</b> <br /> CPL, CDH, CROM, CJC, CHAA </td>
+		  <td><b>Jeudi <br /> 21/06</b> <br /> Chimacienne, CEBULB, CECS, CELB </td>
+		  <td><b>Vendredi <br /> 22/06</b> <br /> ACE, LIBREX </td>
 		</tr>
 		<tr>
-		  <td> <b>Lundi 13/02</b> <br /> C$ </td>
-		  <td> <b>Mardi 14/02</b> <br /> CECS </td>
-		  <td> <b>Mercredi 15/02</b> <br /> CHAA </td>
-		  <td> <b>Jeudi 16/02</b> <br /> CD </td>
-		</tr>
-		<tr>
-		  <td> <b>Lundi 20/02</b> <br /> Semeur </td>
-		  <td> <b>Mardi 21/02</b> <br /> / </td>
-		  <td> <b>Mercredi 22/02</b> <br /> SEMEUR </td>
-		  <td> <b>Jeudi 23/02</b> <br /> CPSY </td>
-		</tr>
-		<tr>
-		  <td> <b>Lundi 27/02</b> <br /> CGEO </td>
-		  <td> <b>Mardi 28/02</b> <br /> CIG </td>
-		  <td> <b>Mercredi 29/02</b> <br /> ACE </td>
-		  <td> <b></b> <br />  </td>
+		  <td><b>Lundi <br /> 25/06</b> <br /> CK, CPSY, CIG, FRONTA </td>
+		  <td><b>Mardi <br /> 26/06</b> <br /> CD, CPS, CARé, ISEP </td>
+		  <td><b>Mercredi <br /> 27/06</b> <br /> CI, CdS, AGRO, CGEO </td>
+		  <td><b>Jeudi <br /> 28/06</b> <br /> SEMEUR, Namuroise, La Lux </td>
+		  <td><b>Vendredi <br /> 29/06</b> <br /> C$, CP, CM, CEPHA </td>
 		</tr>
 	  </tbody>
 	</table>
-	<h2>Mars</h2>
-	<table class="table table-bordered table-striped">
-	  <tbody>
-		<tr>
-		  <td> <b></b> <br />  </td>
-		  <td> <b></b> <br />  </td>
-		  <td> <b></b> <br />  </td>
-		  <td> <b>Jeudi 01/03</b> <br /> CROM </td>
-		</tr>
-		<tr>
-		  <td> <b>Lundi 05/03</b> <br /> CHIMAY </td>
-		  <td> <b>Mardi 06/03</b> <br /> ISEP </td>
-		  <td> <b>Mercredi 07/03</b> <br /> NAMUR </td>
-		  <td> <b>Jeudi 08/03</b> <br /> CHAA </td>
-		</tr>
-		<tr>
-		  <td> <b>Lundi 12/03</b> <br /> CI </td>
-		  <td> <b>Mardi 13/03</b> <br /> CP </td>
-		  <td> <b>Mercredi 14/03</b> <br /> CPS/CPL </td>
-		  <td> <b>Jeudi 15/03</b> <br /> AGRO </td>
-		</tr>
-		<tr>
-		  <td> <b>Lundi 19/03</b> <br /> LIBREX </td>
-		  <td> <b>Mardi 20/03</b> <br /> C$ </td>
-		  <td> <b>Mercredi 21/03</b> <br /> LA LUX </td>
-		  <td> <b>Jeudi 22/03</b> <br /> CARé </td>
-		</tr>
-		<tr>
-		  <td> <b>Lundi 26/03</b> <br /> CdS </td>
-		  <td> <b>Mardi 27/03</b> <br /> CM </td>
-		  <td> <b>Mercredi 28/03</b> <br /> CEBULB </td>
-		  <td> <b>Jeudi 29/03</b> <br /> FRONTA </td>
-		</tr>
-	  </tbody>
-	</table>
-	<h2>Avril</h2>
-	<table class="table table-bordered table-striped">
-	  <tbody>
-		<tr>
-		  <td> <b>Lundi 16/04</b> <br /> ISEP </td>
-		  <td> <b>Mardi 17/04</b> <br /> CJC </td>
-		  <td> <b>Mercredi 18/04</b> <br /> CARé </td>
-		  <td> <b>Jeudi 19/04</b> <br /> LUXOS </td>
-		</tr>
-		<tr>
-		  <td> <b>Lundi 23/04</b> <br /> CdS </td>
-		  <td> <b>Mardi 24/04</b> <br /> CK + PSYCHO </td>
-		  <td> <b>Mercredi 25/04</b> <br /> C$ </td>
-		  <td> <b>Jeudi 26/04</b> <br /> AGRO </td>
-		</tr>
-	  </tbody>
-	</table>
-	<h2>Mai</h2>
-	<table class="table table-bordered table-striped">
-	  <tbody>
-		<tr>
-		  <td> <b></b> <br />  </td>
-		  <td> <b></b> <br />  </td>
-		  <td> <b>Mercredi 02/05</b> <br /> CPS </td>
-		  <td> <b>Jeudi 03/05</b> <br /> CEBULB </td>
-		</tr>
-		<tr>
-		  <td> <b>Lundi 07/05</b> <br /> CdS </td>
-		  <td> <b>Mardi 08/05</b> <br /> C$ </td>
-		  <td> <b>Mercredi 09/05</b> <br /> LIBREX </td>
-		  <td> <b>Jeudi 10/05</b> <br /> CD </td>
-		</tr>
-	  </tbody>
-	</table>	
+	<!-- <h2>Février</h2> -->
+	<!-- <table class="table table-bordered table-striped"> -->
+	<!--   <tbody> -->
+	<!-- 	<tr> -->
+	<!-- 	  <td> <b>Lundi 06/02</b> <br /> Kiné </td> -->
+	<!-- 	  <td> <b>Mardi 07/02</b> <br /> Pharma </td> -->
+	<!-- 	  <td> <b>Mercredi 08/02</b> <br /> CPS-Philo </td> -->
+	<!-- 	  <td> <b>Jeudi 09/02</b> <br /> CdH </td> -->
+	<!-- 	</tr> -->
+	<!-- 	<tr> -->
+	<!-- 	  <td> <b>Lundi 13/02</b> <br /> C$ </td> -->
+	<!-- 	  <td> <b>Mardi 14/02</b> <br /> CECS </td> -->
+	<!-- 	  <td> <b>Mercredi 15/02</b> <br /> CHAA </td> -->
+	<!-- 	  <td> <b>Jeudi 16/02</b> <br /> CD </td> -->
+	<!-- 	</tr> -->
+	<!-- 	<tr> -->
+	<!-- 	  <td> <b>Lundi 20/02</b> <br /> Semeur </td> -->
+	<!-- 	  <td> <b>Mardi 21/02</b> <br /> / </td> -->
+	<!-- 	  <td> <b>Mercredi 22/02</b> <br /> SEMEUR </td> -->
+	<!-- 	  <td> <b>Jeudi 23/02</b> <br /> CPSY </td> -->
+	<!-- 	</tr> -->
+	<!-- 	<tr> -->
+	<!-- 	  <td> <b>Lundi 27/02</b> <br /> CGEO </td> -->
+	<!-- 	  <td> <b>Mardi 28/02</b> <br /> CIG </td> -->
+	<!-- 	  <td> <b>Mercredi 29/02</b> <br /> ACE </td> -->
+	<!-- 	  <td> <b></b> <br />  </td> -->
+	<!-- 	</tr> -->
+	<!--   </tbody> -->
+	<!-- </table> -->
+	<!-- <h2>Mars</h2> -->
+	<!-- <table class="table table-bordered table-striped"> -->
+	<!--   <tbody> -->
+	<!-- 	<tr> -->
+	<!-- 	  <td> <b></b> <br />  </td> -->
+	<!-- 	  <td> <b></b> <br />  </td> -->
+	<!-- 	  <td> <b></b> <br />  </td> -->
+	<!-- 	  <td> <b>Jeudi 01/03</b> <br /> CROM </td> -->
+	<!-- 	</tr> -->
+	<!-- 	<tr> -->
+	<!-- 	  <td> <b>Lundi 05/03</b> <br /> CHIMAY </td> -->
+	<!-- 	  <td> <b>Mardi 06/03</b> <br /> ISEP </td> -->
+	<!-- 	  <td> <b>Mercredi 07/03</b> <br /> NAMUR </td> -->
+	<!-- 	  <td> <b>Jeudi 08/03</b> <br /> CHAA </td> -->
+	<!-- 	</tr> -->
+	<!-- 	<tr> -->
+	<!-- 	  <td> <b>Lundi 12/03</b> <br /> CI </td> -->
+	<!-- 	  <td> <b>Mardi 13/03</b> <br /> CP </td> -->
+	<!-- 	  <td> <b>Mercredi 14/03</b> <br /> CPS/CPL </td> -->
+	<!-- 	  <td> <b>Jeudi 15/03</b> <br /> AGRO </td> -->
+	<!-- 	</tr> -->
+	<!-- 	<tr> -->
+	<!-- 	  <td> <b>Lundi 19/03</b> <br /> LIBREX </td> -->
+	<!-- 	  <td> <b>Mardi 20/03</b> <br /> C$ </td> -->
+	<!-- 	  <td> <b>Mercredi 21/03</b> <br /> LA LUX </td> -->
+	<!-- 	  <td> <b>Jeudi 22/03</b> <br /> CARé </td> -->
+	<!-- 	</tr> -->
+	<!-- 	<tr> -->
+	<!-- 	  <td> <b>Lundi 26/03</b> <br /> CdS </td> -->
+	<!-- 	  <td> <b>Mardi 27/03</b> <br /> CM </td> -->
+	<!-- 	  <td> <b>Mercredi 28/03</b> <br /> CEBULB </td> -->
+	<!-- 	  <td> <b>Jeudi 29/03</b> <br /> FRONTA </td> -->
+	<!-- 	</tr> -->
+	<!--   </tbody> -->
+	<!-- </table> -->
+	<!-- <h2>Avril</h2> -->
+	<!-- <table class="table table-bordered table-striped"> -->
+	<!--   <tbody> -->
+	<!-- 	<tr> -->
+	<!-- 	  <td> <b>Lundi 16/04</b> <br /> ISEP </td> -->
+	<!-- 	  <td> <b>Mardi 17/04</b> <br /> CJC </td> -->
+	<!-- 	  <td> <b>Mercredi 18/04</b> <br /> CARé </td> -->
+	<!-- 	  <td> <b>Jeudi 19/04</b> <br /> LUXOS </td> -->
+	<!-- 	</tr> -->
+	<!-- 	<tr> -->
+	<!-- 	  <td> <b>Lundi 23/04</b> <br /> CdS </td> -->
+	<!-- 	  <td> <b>Mardi 24/04</b> <br /> CK + PSYCHO </td> -->
+	<!-- 	  <td> <b>Mercredi 25/04</b> <br /> C$ </td> -->
+	<!-- 	  <td> <b>Jeudi 26/04</b> <br /> AGRO </td> -->
+	<!-- 	</tr> -->
+	<!--   </tbody> -->
+	<!-- </table> -->
+	<!-- <h2>Mai</h2> -->
+	<!-- <table class="table table-bordered table-striped"> -->
+	<!--   <tbody> -->
+	<!-- 	<tr> -->
+	<!-- 	  <td> <b></b> <br />  </td> -->
+	<!-- 	  <td> <b></b> <br />  </td> -->
+	<!-- 	  <td> <b>Mercredi 02/05</b> <br /> CPS </td> -->
+	<!-- 	  <td> <b>Jeudi 03/05</b> <br /> CEBULB </td> -->
+	<!-- 	</tr> -->
+	<!-- 	<tr> -->
+	<!-- 	  <td> <b>Lundi 07/05</b> <br /> CdS </td> -->
+	<!-- 	  <td> <b>Mardi 08/05</b> <br /> C$ </td> -->
+	<!-- 	  <td> <b>Mercredi 09/05</b> <br /> LIBREX </td> -->
+	<!-- 	  <td> <b>Jeudi 10/05</b> <br /> CD </td> -->
+	<!-- 	</tr> -->
+	<!--   </tbody> -->
+	<!-- </table> -->	
   </div>
 </div>
