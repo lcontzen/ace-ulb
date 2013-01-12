fe4decd140addfb6d88057ab02f7f40ad1afda22
Laurent Contzen <lcontzen@gmail.com>
2013-01-12 18:39:42 +0100

    Modified page/ntv.php content
    Added pictures for new pages/ntv.php content

--- application/views/page/ntv.php
+++ application/views/page/ntv.php
@@ -3,22 +3,55 @@
 </div>
 <div class="row">
   <div class="span11">
-	<p>L'Association des Cercles Etudiants, le Cercle du Libre-Examen l'Union des Anciens Etudiants et l'Ordre Théodore Verhaegen ont le plaisir de vous convier à sa NUIT THEODORE VERHAEGEN dans le cadre superbement restauré de l'ARSENAL d'Etterbeek ! </p>
-	<p>Précédée du banquet (complet) à 19h, la soirée débutera à 22h <br />
-	  Préventes :
-	  <ul>
-		<li>Etudiant : 9 € (+ 2€ sur place)</li>
-		<li>Bourgeois : 11 € (+ 2€ sur place)</li>
-	  </ul>
-	  Préventes disponibles : <br />
-	  <ul>
-		<li>De 12h à 16h, au bâtiment F1</li>	
-		<li>De 18h à 19h, au local de l'ACE</li>	
-	  </ul>
-	  Les non-étudiants peuvent également réserver leur prévente par mail (nuit.theodore.verhaegen@gmail.com) jusqu'au vendredi 2/12, 18h. Elles peuvent ensuite être payées sur le compte BE61 001-0810667-17.
-	</p>
+	<p>L'Association des Cercles Etudiants, le Cercle du Libre-Examen, l'Union des Anciens Etudiants et l'Ordre Théodore Verhaegen ont le plaisir de vous convier à la NUIT THEODORE VERHAEGEN qui, cette année, aura lieu au Birmingham Palace. <br />
+	  (Station métro Delacroix à côté de la salle)</p>
+	<h3>Programme de la soirée :</h3>
+	<ul>
+	  <li>19h30 : Apéritif : Zakouskis accompagnés de leurs bulles.</li>
+	  <li>20h00 : Banquet.</li>
+	  <li>22h30 : Début du bal.</li>
+	  <li>23h : Remise des vlecks de l'Ordre Théodore Verhaegen et de l'Ordre des Lumières.</li>
+	</ul>
+	<h3>Informations pratiques :</h3>
+	<p>Banquet : Etudiant 35€ / Bourgeois 50€ <br />
+	  Réservation jusqu'au mercredi 28/11! <br />
+	  Pour réserver une table au banquet, merci de nous envoyer un mail à "reservations.ntv@gmail.com" avec les noms, prénoms et choix de menu pour chaque personne composant votre table. (Minimum 6 pers. pour une table de 8 et 8 pers. pour une table de 10). <br />
+	  PAYEMENT SUR LE COMPTE : BE61 001-0810667-17 <br />
+	  COMMUNICATION : NOM + PRENOM + BANQUET NTV <br />
+	  Attention : Places limitées à une centaine.</p>
+
+	<h5>Menu du banquet (à composer selon votre choix) :<h5>
+	<p>Entrées :</p>
+	<ul>
+	  <li>Carpaccio de bœuf parsemé de copeaux de parmesan et de jeunes pousses de roquette. (A1)</li>
+	  <li>Fines tranches de tomates avec sa mozzarella fraîche et son filet d'huile d'olive, assaisonné à l'italienne. (A2)</li>
+	</ul>
+
+	<p>Plats :</p>
+	<ul>
+	  <li>Filet de poulet sauce Archiduc, jardinière de légumes et gratin dauphinois (B1)</li>
+	  <li>Darne de saumon sauce moutarde vin blanc, jardinière de légumes, gratin dauphinois (B2)</li>
+	</ul>
+
+	<p>Dessert :</p>
+	<ul>
+	  <li>Salade de fruits frais et sa boule de glace vanille maison.</li>
+	</ul>
+	<p>Le repas sera accompagné de vin blanc et rouge à volonté. </p>
+
+	<p>Attention : La participation au banquet comprend évidemment l'entrée au bal.</p>
+
+	<p>Prévente bal : Etudiant 12 € / Bourgeois 15 € (+ 2€ sur place)</p>
+
+	<p>Préventes disponibles entre les restos du F1 :</p>
+	<ul>
+	  <li>De 12h à 16h à partir du vendredi 23/11</li>
+	  <li>De 12h à 18h le jour même du bal.</li>
+	</ul>
+
+	<p>Navettes au départ du Janson dès 22h00.</p>
   </div>
   <div class="span5">
-	<?php echo HTML::image('public/images/ntv11big.jpg', array('alt' => 'Affiche NTV 2011', 'width' => '375')); ?>
+	<?php echo HTML::image('public/images/ntv12.jpg', array('alt' => 'Affiche NTV 2012', 'width' => '375')); ?>
   </div>  
 </div>
